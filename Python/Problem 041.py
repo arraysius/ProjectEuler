cebd951980780a70094eff026875c84b6f3cbdde
Aloysius Lee <aloy.cp@gmail.com>
2016-12-17 15:43:27 +0800

    Completed Problem 041

--- Python/Problem 041.py
+++ Python/Problem 041.py
@@ -2,30 +2,42 @@ from time import clock
 
 
 def prime_sieve(limit):
-    numbers = [True] * limit
-    numbers[0] = numbers[1] = False
-    primes = []
+	numbers = [True] * limit
+	numbers[0] = numbers[1] = False
+	primes = []
 
-    for (i, isPrime) in enumerate(numbers):
-        if isPrime:
-            primes.append(i)
-            for x in range(i * i, limit, i):
-                numbers[x] = False
-    return primes
+	for (i, isPrime) in enumerate(numbers):
+		if isPrime:
+			primes.append(i)
+			for x in range(i * i, limit, i):
+				numbers[x] = False
+	return primes
 
 
-def isPandigital(n):
-    strn = sorted(str(n))
-
+def digit_freq(num):
+	freq = dict()
+	for d in num:
+		if d in freq:
+			freq[d] += 1
+		else:
+			freq[d] = 1
+	return freq
 
 
 def main():
-    start = clock()
+	start = clock()
 
-    primes = prime_sieve(1000000)
+	largest_pandigital_prime = 0
 
-    print(clock() - start)
+	for prime in prime_sieve(100000000):
+		a = digit_freq(str(prime))
+		b = digit_freq(''.join([str(d) for d in range(1, len(str(prime)) + 1)]))
+		if a == b:
+			largest_pandigital_prime = prime
+
+	print(largest_pandigital_prime)
+	print(clock() - start)
 
 
 if __name__ == '__main__':
-    main()
+	main()
